c32a5182328beb6793401b60934f056bc6b79992
Stefan Buys <22056509+StefanB7@users.noreply.github.com>
2021-05-02 10:24:10 +0200

    Rondspeel

---
+++ stefan/3DES_stefan.py
@@ -0,0 +1,29 @@
+# EHN 410 - Practical 2 - 2021
+# 3DES encryption and decryption
+# Group 7
+# Created: 2 May 2021 by Stefan Buys
+
+import numpy as np
+
+def TDEA_Encrypt(inspect_mode, plaintext, key1, key2, key3, ip):
+    print("3DES Encryption")
+
+def TDEA_Decrypt(inspect_mode, ciphertext, key1, key2, key3, inv_ip):
+    print("3DES Decryption")
+
+permutation = np.load("Practical 2 File package/DES_Initial_Permutation.npy")
+
+
+
+string = "Hello, hoe gaan dit vandag?"
+
+bytearr = np.empty(len(string),dtype=np.byte)
+for i in range(len(string)):
+    bytearr[i] = ord(string[i])
+
+bytearr[0] &= 0xFF
+print(bytearr)
+
+int toets = 0
+
+toets +=
---
+++ stefan/AES_stefan.py
@@ -0,0 +1,13 @@
+# EHN 410 - Practical 2 - 2021
+# AES encryption and decryption
+# Group 7
+# Created: 2 May 2021 by Stefan Buys
+
+import numpy as np
+
+def AES_Encrypt(inspect_mode, plaintext, iv, key, sbox_array):
+    print("AES encryption")
+
+
+def AES_Decrypt(inspect_mode, ciphertext, iv, key, inv_sbox_array):
+    print("AES decryption")
