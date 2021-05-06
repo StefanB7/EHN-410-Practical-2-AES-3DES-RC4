e6b8a9dfe679448ee1f0366cf39679bce9f26aa8
Stefan Buys <22056509+StefanB7@users.noreply.github.com>
2021-05-06 13:04:17 +0200

    Key generation werk en dit lyk asof dit reg is

--- stefan/3DES_stefan.py
+++ stefan/3DES_stefan.py
@@ -95,14 +95,22 @@ def keyGeneration(originalKey, permutationInitial, permutationRound):
     #Calculate the subkeys:
 
     oneBitShifts = [1, 2, 9, 16]
+    subKeys = []
+    statusKey = permutedKey
 
     for keyNum in range(16):
-        if (i+1) in oneBitShifts:
+        if (keyNum+1) in oneBitShifts:
             numshifts = 1
         else:
             numshifts = 2
 
         #The key left shift function works by performing a circular left shift on each half of the key seperately:
+        statusKey = shiftKeyHalvesLeft(statusKey, numshifts)
+
+        #Perform a permutation on the statusKey to get the next subkey:
+        subKeys.append(permutation(statusKey, permutationRound))
+
+    return subKeys
 
 
 
@@ -226,13 +234,14 @@ def shiftKeyHalvesLeft(keyInput, numshifts):
 
 #Toets die key generation:
 
-# initKey = bytearray([0x13,0x34,0x57,0x79,0x9B,0xBC,0xDF,0xF1])
-# permuteKey = [57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4]
-#
-# print(keyGeneration(initKey,permuteKey,[0]))
+initKey = bytearray([0x13,0x34,0x57,0x79,0x9B,0xBC,0xDF,0xF1])
+permuteKey = [57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4]
+permuteRound = [14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32]
 
-testkey = bytearray([0xF0,0xCC,0xAA,0xF5,0x56,0x67,0x8F])
-print(shiftKeyHalvesLeft(testkey,1))
+print(keyGeneration(initKey,permuteKey,permuteRound))
+
+# testkey = bytearray([0xF0,0xCC,0xAA,0xF5,0x56,0x67,0x8F])
+# print(shiftKeyHalvesLeft(testkey,1))
 
 
 
