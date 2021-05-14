085255c63016e461783c499d8d88c35294e6ebf4
Stefan Buys <22056509+StefanB7@users.noreply.github.com>
2021-05-14 23:08:01 +0200

    RC4 Image Encryption and Decryption Done

--- stefan/RC4_stefan.py
+++ stefan/RC4_stefan.py
@@ -32,7 +32,7 @@ def RC4_Enctrypt(plaintext, key):
     for i in range(255):
         index = (index + S[i] + T[i]) % 256
         temp = S[i]
-        
+
         S[i] = S[index]
         S[index] = temp
 
@@ -69,6 +69,67 @@ def RC4_Enctrypt(plaintext, key):
             cipherTextString = cipherTextString + chr(cipherText[i])
 
         return cipherTextString
+
+
+    # If the plaintext is an image (ndarray) that needs to be encrypted:
+    if (isinstance(plaintext, np.ndarray)):
+
+        # Check the plaintext's dimentions:
+        numRows = plaintext.shape[0]
+        numColumns = plaintext.shape[1]
+        numLayers = plaintext.shape[2]
+
+        # Test if there is an AlphaLayer:
+        bAlphaLayer = False
+        if (numLayers > 3):
+            bAlphaLayer = True
+            numLayers = 3
+            alpha_layer = np.array(plaintext[:, :, 3])
+
+        # Ciphertext variable:
+        cipherText = np.zeros((numRows, numColumns, numLayers), dtype='u1')
+
+        #Variables used in the stream cipher should persist over different layer encryption:
+        i = 0
+        j = 0
+
+        for layer in range(numLayers):
+
+            #Create an input plaintext bytearray for the current layer:
+            index = 0
+            plaintextBytes = bytearray(numRows*numColumns)
+            cipherTextBytes = bytearray(numRows*numColumns)
+
+            for i in range(numRows):
+                for j in range(numColumns):
+                    plaintextBytes[index] = plaintext[i][j][layer]
+                    index += 1
+
+            #Encrypt the plaintext:
+            for index in range(len(plaintextBytes)):
+                # Generate the next stream element:
+                i = (i + 1) % 256
+                j = (j + S[i]) % 256
+                temp = S[i]
+                S[i] = S[j]
+                S[j] = temp
+
+                streamElementIndex = (S[i] + S[j]) % 256
+                streamElement = S[streamElementIndex]
+
+                cipherTextBytes[index] = plaintextBytes[index] ^ streamElement
+
+            #Transfer the calculated output to the ciphertext image ndarray variable:
+            index = 0
+            for i in range(numRows):
+                for j in range(numColumns):
+                    cipherText[i][j][layer] = cipherTextBytes[index]
+                    index += 1
+
+        if bAlphaLayer:
+            cipherText = np.dstack((cipherText, alpha_layer))
+
+        return cipherText.astype(int)
 
 
 def RC4_Decrypt(ciphertext, key):
@@ -130,9 +191,84 @@ def RC4_Decrypt(ciphertext, key):
 
         return plainTextString
 
-    print("RC4 Decrypt")
+
+
+    # If the plaintext is an image (ndarray) that needs to be encrypted:
+    if (isinstance(ciphertext, np.ndarray)):
+
+        # Check the plaintext's dimentions:
+        numRows = ciphertext.shape[0]
+        numColumns = ciphertext.shape[1]
+        numLayers = ciphertext.shape[2]
+
+        # Test if there is an AlphaLayer:
+        bAlphaLayer = False
+        if (numLayers > 3):
+            bAlphaLayer = True
+            numLayers = 3
+            alpha_layer = np.array(ciphertext[:, :, 3])
+
+        # Ciphertext variable:
+        plainText = np.zeros((numRows, numColumns, numLayers), dtype='u1')
+
+        # Variables used in the stream cipher should persist over different layer encryption:
+        i = 0
+        j = 0
+
+        for layer in range(numLayers):
+
+            # Create an input plaintext bytearray for the current layer:
+            index = 0
+            cipherTextBytes = bytearray(numRows * numColumns)
+            plainTextBytes = bytearray(numRows * numColumns)
+
+            for i in range(numRows):
+                for j in range(numColumns):
+                    cipherTextBytes[index] = ciphertext[i][j][layer]
+                    index += 1
+
+            # Encrypt the plaintext:
+            for index in range(len(cipherTextBytes)):
+                # Generate the next stream element:
+                i = (i + 1) % 256
+                j = (j + S[i]) % 256
+                temp = S[i]
+                S[i] = S[j]
+                S[j] = temp
+
+                streamElementIndex = (S[i] + S[j]) % 256
+                streamElement = S[streamElementIndex]
+
+                plainTextBytes[index] = cipherTextBytes[index] ^ streamElement
+
+            # Transfer the calculated output to the ciphertext image ndarray variable:
+            index = 0
+            for i in range(numRows):
+                for j in range(numColumns):
+                    plainText[i][j][layer] = plainTextBytes[index]
+                    index += 1
+
+        if bAlphaLayer:
+            cipherText = np.dstack((plainText, alpha_layer))
+
+        return plainText.astype(int)
 
 encrypted = RC4_Enctrypt("Hello, hoe gaan dit met jou?", "stefan")
 print(encrypted)
 decrypted = RC4_Decrypt(encrypted,"stefan")
 print(decrypted)
+
+#Test Image:
+p_File = Image.open('office.png')
+p_img = np.asarray(p_File)
+imgENC = RC4_Enctrypt(p_img, "stefan")
+
+Image.fromarray(imgENC.astype(np.uint8)).save('office_encrypted_rc4.png')
+
+print("Image Encryption Done")
+
+p_File = Image.open('office_encrypted_rc4.png')
+p_img = np.asarray(p_File)
+imgENC = RC4_Decrypt(p_img, "stefan")
+
+Image.fromarray(imgENC.astype(np.uint8)).save('office_decrypted_rc4.png')
